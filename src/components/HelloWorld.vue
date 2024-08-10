5bdbacebc31306c06c661429f05927cf1bf7a1b6
Jonathan Lie <jonathanlie@192.168.1.4>
2024-08-10 23:17:38 +0700

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import BarrierIcon from './icons/IconBarrier.vue'
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">Hello! I'm Jonathan</h1>
+    <h2><BarrierIcon /> UNDER DEVELOPMENT <BarrierIcon /></h2>
+    <br />
+    <h3>In the meantime, please check out the summary about me.</h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h2 {
+  font-size: 1.4rem;
+  color: hsla(0, 100%, 71%, 1);
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h2,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h2,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import EngineerIcon from './icons/IconEngineer.vue'
+import CybersecurityIcon from './icons/IconCybersecurity.vue'
+import PianistIcon from './icons/IconPianist.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <EngineerIcon />
+    </template>
+    <template #heading>Software Engineer</template>
+
+    With over four years of experience, I have completed various projects using the PERN stack
+    (PostgreSQL, Express.js, React, Node.js) with TypeScript. I am currently working as a full-time
+    Software Engineer at
+    <a href="https://www.kodefox.com/" target="_blank">KodeFox, Inc</a>.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CybersecurityIcon />
+    </template>
+    <template #heading>Cybersecurity Enthusiast</template>
+
+    I have a bachelor's degree in Computer Science with a focus on Cybersecurity. I also obtained a
+    certification in 2020. Although I am currently a Software Engineer, I remain passionate about
+    cybersecurity and related fields.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <PianistIcon />
+    </template>
+    <template #heading>Musician</template>
+
+    I started my music journey as a hobby and have always enjoyed playing the piano. I am now at
+    Grade Four at my local music school. Additionally, I play piano at my local church services.
+  </WelcomeItem>
+</template>
